3d66df478a50143c7a198dbe4a2bb9a92b93f273
alliza <bistaaliza@gmail.com>
2017-11-21 04:52:29 -0600

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+   
+        $("#pa").hide();
+		$("#pa1").hide();
+			$("#pa3").hide();
+   
+    $("#show").click(function(){
+        $("#pa").toggle();
+    });
+	
+	$("#show1").click(function(){
+        $("#pa1").toggle();
+    });
+});
